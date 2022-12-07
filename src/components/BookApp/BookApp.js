f8935c94ab36c0ef1e3ab60d856647d7876d951a
Vaishakbg <vaishakbg@gmail.com>
2022-12-07 17:57:15 +0530

    Add Book to Array

--- src/components/BookApp/BookApp.js
+++ src/components/BookApp/BookApp.js
@@ -5,11 +5,17 @@ import BookCreate from './BookCreate'
 function BookApp() {
   const [books, setBooks] = useState([]);
   const createBook =(title) => {
-    console.log('Add Book', title);
     //BAD CODE!
+    // books.push({id: 123, title});
+    // setBooks(books);
+    const id = Math.round(Math.random() * 9999)
+    const updatedBooks = [...books, { id, title }]
+    setBooks(updatedBooks);
+    console.log(books);
   }
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   )
