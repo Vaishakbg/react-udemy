a94b298f128bb806953b7bff468e5e135ddec157
Vaishakbg <vaishakbg@gmail.com>
2022-12-07 19:10:34 +0530

    Handling Form - Completed

--- src/components/BookApp/BookApp.js
+++ src/components/BookApp/BookApp.js
@@ -28,6 +28,7 @@ function BookApp() {
   }
   return (
     <div className='app'>
+      <h1>Reading List</h1>
       <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
       <BookCreate onCreate={createBook} />
     </div>
--- src/components/BookApp/BookShow.js
+++ src/components/BookApp/BookShow.js
@@ -17,6 +17,7 @@ function BookShow({book, onDelete, onEdit}) {
   if(showEdit) content = <BookEdit book={book} onSubmit={handleOnSubmit} />
   return (
     <div className='book-show'>
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt={book.title} />
       <div>{content}</div>
       <div className="actions">
       <button className='edit' onClick={handleEditClick}>Edit</button>
