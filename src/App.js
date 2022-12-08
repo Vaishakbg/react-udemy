9ec4b0f00821a81c5d54169a9fbccdce9ca7c80a
Vaishakbg <vaishakbg@gmail.com>
2022-12-08 21:32:34 +0530

    Children Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import 'bulma/css/bulma.css';
+import RouterApp from "./RouterApp/RouterApp";
 
 function App() {
   return (
       <div>
-        App
+        <RouterApp />
       </div>
   );
 }
---
+++ src/RouterApp/RouterApp.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './Button'
+
+function RouterApp() {
+  return (
+    <div>
+        <div>
+            <Button>Primary</Button>
+        </div>
+        <div>
+            <Button>Secondary</Button>
+        </div>
+        <div>
+            <Button>Bye Now!</Button>
+        </div>
+        <div>
+            <Button>Click Heare!</Button>
+        </div>
+        <div>
+            <Button>Hi</Button>
+        </div>
+    </div>
+  )
+}
+
+export default RouterApp
