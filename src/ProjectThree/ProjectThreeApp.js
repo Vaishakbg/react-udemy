722d11218426f6accbe06e54c571bf9c55006b6c
Vaishakbg <vaishakbg@gmail.com>
2022-12-09 22:20:54 +0530

    Accordion start

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import Accordion from './components/Accordion';
 import ButtonPage from "./pages/ButtonPage";
 
 
 function ProjectThreeApp() {
+  const items = [
+    {
+      id:'sdfds',
+      label: 'Can I use React on a project?',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
+    },
+    {
+      id:'guytutyuy',
+      label: 'Can I use Javascript on a project?',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
+    },
+    {
+      id:'bcvreer',
+      label: 'Can I use CSS on a project?',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
+    }
+  ]
   return (
     <div>
-        <ButtonPage />
+        {/* <ButtonPage /> */}
+        <Accordion items={items} />
     </div>
   )
 }
