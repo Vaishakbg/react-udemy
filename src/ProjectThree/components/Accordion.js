56f7eaeb5ab94964a1b516f0c42b8b7b86dcf6c8
Vaishakbg <vaishakbg@gmail.com>
2022-12-13 21:34:02 +0530

    Adding styling to accordion

--- src/ProjectThree/components/Accordion.js
+++ src/ProjectThree/components/Accordion.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import {GoChevronDown, GoChevronLeft} from "react-icons/go"
 function Accordion({items}) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
@@ -8,16 +8,22 @@ function Accordion({items}) {
   }
     const renderedItems = items.map((item, index)=>{
       const isExpanded = index === expandedIndex;
-
+      const icon = <span className='text-2xl'>
+        {isExpanded ? <GoChevronDown />: <GoChevronLeft />}
+      </span>
         return(
            <div key={item.id}>
-                <div onClick={()=> handleClick(index)}>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                <div className='flex p-3 bg-gray-50 border-b items-center justify-between cursor-pointer' onClick={()=> handleClick(index)}>
+                  {item.label}
+                  {icon}
+                </div>
+                
+                {isExpanded && <div className='border-b p-5'>{item.content}</div>}
            </div> 
         )
     })
   return (
-    <div>
+    <div className='border-x border-t rounded'>
         {renderedItems}
     </div>
   )
