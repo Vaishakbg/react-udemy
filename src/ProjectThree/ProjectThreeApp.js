0c23e468b9e8c56d23a880dbf6cf8241b51d5786
Vaishakbg <vaishakbg@gmail.com>
2022-12-13 22:07:16 +0530

    Completed AccordionPage started Dropdown

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -1,30 +1,12 @@
 import React from 'react'
-import Accordion from './components/Accordion';
-import ButtonPage from "./pages/ButtonPage";
+import Dropdown from './components/Dropdown'
 
 
 function ProjectThreeApp() {
-  const items = [
-    {
-      id:'sdfds',
-      label: 'Can I use React on a project?',
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
-    },
-    {
-      id:'guytutyuy',
-      label: 'Can I use Javascript on a project?',
-      content: 'Sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse.'
-    },
-    {
-      id:'bcvreer',
-      label: 'Can I use CSS on a project?',
-      content: 'Lorem ipsum met temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
-    }
-  ]
+  
   return (
     <div>
-        {/* <ButtonPage /> */}
-        <Accordion items={items} />
+      <Dropdown />
     </div>
   )
 }
---
+++ src/ProjectThree/pages/AccordionPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Accordion from '../components/Accordion';
+
+
+function AccordionPage() {
+  const items = [
+    {
+      id:'sdfds',
+      label: 'Can I use React on a project?',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
+    },
+    {
+      id:'guytutyuy',
+      label: 'Can I use Javascript on a project?',
+      content: 'Sit amet consectetur adipisicing elit. Amet temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse.'
+    },
+    {
+      id:'bcvreer',
+      label: 'Can I use CSS on a project?',
+      content: 'Lorem ipsum met temporibus quas suscipit eos ipsum nam corrupti odit, quaerat sint. Error esse dolore eius id corporis animi consequatur, quas praesentium cupiditate'
+    }
+  ]
+  return (
+    <div>
+        <Accordion items={items} />
+    </div>
+  )
+}
+
+export default AccordionPage
