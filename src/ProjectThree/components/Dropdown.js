aa17ad1a7bdf455506db75d1bf16ca093e0224e7
Vaishakbg <vaishakbg@gmail.com>
2022-12-14 19:28:15 +0530

    Added styles to Dropdown

--- src/ProjectThree/components/Dropdown.js
+++ src/ProjectThree/components/Dropdown.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {GoChevronDown} from 'react-icons/go'
 
 function Dropdown({options, value, onChange}) {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,14 +14,17 @@ function Dropdown({options, value, onChange}) {
   }
 
   const renderedOptions = options.map((option)=> {
-    return <div key={option.value} onClick={()=>handleOptionClick(option)}>{option.name}</div>
+    return <div className='hover:bg-sky-100 rounded cursor-pointer p-1' key={option.value} onClick={()=>handleOptionClick(option)}>{option.name}</div>
   });
 
   
   return (
-    <div>
-      <div onClick={handleClick}>{value?.name || 'Select...'}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className='w-48 relative'>
+      <div className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full' onClick={handleClick}>
+        {value?.name || 'Select...'}
+        <GoChevronDown className='text-lg'/>
+      </div>
+      {isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
     </div>
   )
 }
