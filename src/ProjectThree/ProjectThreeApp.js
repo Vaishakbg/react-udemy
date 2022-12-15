21a9813ee7ed60064ec3e71d71f6600e53c9f480
Vaishakbg <vaishakbg@gmail.com>
2022-12-15 19:53:41 +0530

    Custom Route Component

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Link from './components/Link'
+import Route from './components/Route'
+import AccordionPage from './pages/AccordionPage';
+import ButtonPage from './pages/ButtonPage';
+import DropdownPage from './pages/DropdownPage';
 
 function ProjectThreeApp() {
  return (
     <div className=''>
       <Link to={'/accordion'}>Accordion</Link>
+      <Link to={'/button'}>Button</Link>
       <Link to={'/dropdown'}>Dropdown</Link>
+      <div>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/button'>
+          <ButtonPage />
+        </Route>
+        <Route path='/dropdown'>
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   )
 }
