2f855ff5beafc3f0de8a9b964525873b9830270c
Vaishakbg <vaishakbg@gmail.com>
2022-12-15 19:19:07 +0530

    eventlistener popstate

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function ProjectThreeApp() {
   //window.history.pushState({},'','/dropdown'); Add path to URL without refreshing page
+  //window.addEventListener('popstate',()=>console.log('Im at',window.location.pathname));
   return (
     <div className=''>
       App
