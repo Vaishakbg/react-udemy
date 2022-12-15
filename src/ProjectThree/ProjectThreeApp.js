254ab50ddae174589585030842bf99e22d17feef
Vaishakbg <vaishakbg@gmail.com>
2022-12-15 18:26:53 +0530

    Moved code to DropdownPage

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -1,22 +1,11 @@
-import React, { useState } from 'react'
-import Dropdown from './components/Dropdown'
+import React from 'react'
+import DropdownPage from './pages/DropdownPage'
 
 
 function ProjectThreeApp() {
-  const [selection, setSelection] = useState(null)
-  const options = [
-    {name:'Red', value:'red'},
-    {name:'Green', value:'green'},
-    {name:'Blue', value:'blue'},
-  ];
-
-  const handleSelection=(option)=>{
-    setSelection(option)
-  }
   return (
-    <div className='flex'>
-      <Dropdown options={options} value={selection} onChange={handleSelection} />
-      <Dropdown options={options} value={selection} onChange={handleSelection} />
+    <div className=''>
+      <DropdownPage />
     </div>
   )
 }
---
+++ src/ProjectThree/pages/DropdownPage.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Dropdown from '../components/Dropdown'
+
+
+function DropdownPage() {
+  const [selection, setSelection] = useState(null)
+  const options = [
+    {name:'Red', value:'red'},
+    {name:'Green', value:'green'},
+    {name:'Blue', value:'blue'},
+  ];
+
+  const handleSelection=(option)=>{
+    setSelection(option)
+  }
+  return (
+    <div className='flex'>
+      <Dropdown options={options} value={selection} onChange={handleSelection} />
+    </div>
+  )
+}
+
+export default DropdownPage
