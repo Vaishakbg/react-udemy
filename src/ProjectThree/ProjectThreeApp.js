57010ce7c477d8d45fcdd3247c006fd735518c69
Vaishakbg <vaishakbg@gmail.com>
2022-12-19 20:11:39 +0530

    Modal Component

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -4,10 +4,11 @@ import Sidebar from './components/Sidebar';
 import AccordionPage from './pages/AccordionPage';
 import ButtonPage from './pages/ButtonPage';
 import DropdownPage from './pages/DropdownPage';
+import ModalPage from './pages/ModalPage';
 
 function ProjectThreeApp() {
  return (
-    <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+    <div className='mx-auto grid grid-cols-6 gap-4 mt-4'>
       <Sidebar />
       <div className='col-span-5'>
         <Route path='/accordion'>
@@ -18,6 +19,9 @@ function ProjectThreeApp() {
         </Route>
         <Route path='/'>
           <DropdownPage />
+        </Route>
+        <Route path='/modal'>
+          <ModalPage />
         </Route>
       </div>
     </div>
---
+++ src/ProjectThree/pages/ModalPage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Button from '../components/Button'
+import Modal from '../components/Modal'
+
+function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick=()=>{
+        setShowModal(true);
+    }
+  return (
+    <div>
+        <Button primary onClick={handleClick}>Open Modal</Button>
+        {showModal && <Modal />}
+    </div>
+  )
+}
+
+export default ModalPage
