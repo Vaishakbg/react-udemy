1edd8ca3e488138d092fe76beded3ae574132d1c
Vaishakbg <vaishakbg@gmail.com>
2022-12-20 20:19:43 +0530

    ***Formatting

--- src/ProjectThree/components/Table.js
+++ src/ProjectThree/components/Table.js
@@ -1,18 +1,20 @@
 import React, { Fragment } from "react";
 
 function Table({ data, config, keyFn }) {
-    const renderedHeaders = config.map((column) => {
-          if (column.header) {
-            return <Fragment key={column.label}>{column.header()}</Fragment>;
-          }
-          return <th key={column.label}>{ column.label }</th>
-      });
+  const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
+    return <th key={column.label}>{column.label}</th>;
+  });
   const renderedRows = data.map((rowData) => {
-    const renderedCells = config.map((column)=>{
-        return (
-            <td className="p-2" key={column.label}>{column.render(rowData)}</td>
-        )
-    })
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
     return (
       <tr className="border-b" key={keyFn(rowData)}>
         {renderedCells}
@@ -22,9 +24,7 @@ function Table({ data, config, keyFn }) {
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          {renderedHeaders}
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
