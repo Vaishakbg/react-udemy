e6ace21c7a56d32833669f7773bdd975e37e810c
Vaishakbg <vaishakbg@gmail.com>
2022-12-20 19:53:49 +0530

    javascript sorting

--- src/ProjectThree/pages/TablePage.js
+++ src/ProjectThree/pages/TablePage.js
@@ -12,21 +12,32 @@ function TablePage() {
   const config = [
     {
       label: "Fruits",
-      render: (fruit)=>fruit.name,
+      render: (fruit) => fruit.name,
     },
-    { 
+    {
       label: "Color",
-      render: (fruit)=><div className={`p-3 m-2 ${fruit.color}`}></div>,
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
-    { 
+    {
       label: "Score",
-      render: (fruit)=>fruit.score,
+      render: (fruit) => fruit.score,
     },
   ];
 
-  const keyFn = (fruit)=>{
+  const keyFn = (fruit) => {
     return fruit.name;
+  };
+
+  function init(){
+    const data = [5,3,2,10];
+    console.log(data.sort());
+    console.log(data.sort((a,b)=> a-b));
+    const str=["t","A","c","a"]
+    console.log(str.sort());
+    console.log(str.sort((a,b)=> a.localeCompare(b)));
   }
+  init();
+
   return (
     <div>
       <Table data={data} config={config} keyFn={keyFn} />
