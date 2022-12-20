b72d20702a4a5e13d7110510f488a9d200d64e5d
Vaishakbg <vaishakbg@gmail.com>
2022-12-20 20:18:55 +0530

    React Fragment

--- src/ProjectThree/components/Table.js
+++ src/ProjectThree/components/Table.js
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 function Table({ data, config, keyFn }) {
     const renderedHeaders = config.map((column) => {
           if (column.header) {
-            return<Fragment key={column.label}>{column.header()}</Fragment>;
+            return <Fragment key={column.label}>{column.header()}</Fragment>;
           }
           return <th key={column.label}>{ column.label }</th>
       });
