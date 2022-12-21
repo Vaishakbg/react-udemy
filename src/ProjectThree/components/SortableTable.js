abfe39423d99f6db8cb814db48fbc63e46e0cfbe
Vaishakbg <vaishakbg@gmail.com>
2022-12-21 18:27:19 +0530

    Adding Sort state

--- src/ProjectThree/components/SortableTable.js
+++ src/ProjectThree/components/SortableTable.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import Table from './Table'
+import React, { useState } from "react";
+import Table from "./Table";
 
 function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
   const { config } = props;
 
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
   const updatedConfig = config.map((column) => {
-    if(!column.sortValue) {
-        return column;
+    if (!column.sortValue) {
+      return column;
     }
 
     return {
-        ...column,
-        header:()=> <th>{column.label} IS Sortable</th>
-    }
-  })
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} IS Sortable
+        </th>
+      ),
+    };
+  });
   return (
-    <Table {...props} config={updatedConfig} />
-  )
+    <div>
+        {sortBy} - {sortOrder}
+        <Table {...props} config={updatedConfig} />
+    </div>
+  );
 }
 
-export default SortableTable
+export default SortableTable;
