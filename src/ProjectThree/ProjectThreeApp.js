b5114cfd766764c21402d1904bb88752dcdbe184
Vaishakbg <vaishakbg@gmail.com>
2023-01-11 11:55:25 +0530

    Counter component

--- src/ProjectThree/ProjectThreeApp.js
+++ src/ProjectThree/ProjectThreeApp.js
@@ -6,6 +6,7 @@ import ButtonPage from './pages/ButtonPage';
 import DropdownPage from './pages/DropdownPage';
 import ModalPage from './pages/ModalPage';
 import TablePage from './pages/TablePage';
+import CounterPage from './pages/CounterPage';
 
 function ProjectThreeApp() {
  return (
@@ -26,6 +27,9 @@ function ProjectThreeApp() {
         </Route>
         <Route path='/table'>
           <TablePage />
+        </Route>
+        <Route path='/counter'>
+          <CounterPage initialCount={10} />
         </Route>
       </div>
     </div>
