f9f712a211b868d5811ef79134ba6c6c72ad15bf
Vaishak <vaishakbg@gmail.com>
2023-01-27 10:07:42 +0000

    Redux - Accessing state in a component

--- src/ProjectRedux/components/SongPlaylist.js
+++ src/ProjectRedux/components/SongPlaylist.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createRandomSong } from '../data';
 import { addSong } from '../store';
 
 function SongPlaylist() {
   const dispatch = useDispatch();
-  // To Do:
+
   // Get list of songs
-  const songPlaylist = [];
+  const songPlaylist = useSelector((state)=>{
+    return state.songs;
+  })
 
   const handleSongAdd = (song) => {
     const action = addSong(song)
     dispatch(action);
   };
+
   const handleSongRemove = (song) => {
     // To Do:
     // Remove song from list of songs
