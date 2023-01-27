5a378210afb433910b3c82c3d86192beb45644bc
Vaishak <vaishakbg@gmail.com>
2023-01-27 12:01:01 +0000

    Updates to files !!!!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ProjectRedux from "./ProjectRedux/ProjectRedux";
-import "bulma/css/bulma.css";
-import { store } from "./ProjectRedux/store";
-import { Provider } from "react-redux";
+// import "bulma/css/bulma.css";
 // import ProjectThreeApp from "./ProjectThree/ProjectThreeApp";
 
 function App() {
   return (
-    <Provider store={store}>
       <div>
         {/* <ProjectThreeApp /> */}
         <ProjectRedux />
       </div>
-    </Provider>
   );
 }
 
--- src/ProjectRedux/ProjectRedux.js
+++ src/ProjectRedux/ProjectRedux.js
@@ -1,26 +1,16 @@
+import "bulma/css/bulma.css";
 import "./style.css";
-import React from 'react';
-import MoviePlaylist from './components/MoviePlaylist';
-import SongPlaylist from './components/SongPlaylist';
-import { useDispatch } from "react-redux";
-import { reset } from "./store";
+import React from "react";
+import ProjectReduxApp from "./ProjectReduxApp";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 function ProjectRedux() {
-  const dispatch = useDispatch();
-    const handleResetClick = () => {
-        dispatch(reset());
-      };
   return (
-    <div className="container is-fluid">
-      <button onClick={() => handleResetClick()} className="button is-danger">
-        Reset Both Playlists
-      </button>
-      <hr />
-      <MoviePlaylist />
-      <hr />
-      <SongPlaylist />
-    </div>
-  )
+    <Provider store={store}>
+      <ProjectReduxApp />
+    </Provider>
+  );
 }
 
-export default ProjectRedux
+export default ProjectRedux;
