2b74fedb73c2c99167b036a127c1594410174d19
Vaishak <vaishakbg@gmail.com>
2023-01-31 05:58:18 +0000

    Redux - Delete Records and adding css

--- src/ProjectCars/ProjectCarsApp.js
+++ src/ProjectCars/ProjectCarsApp.js
@@ -1,7 +1,8 @@
 import React from "react";
+import './ProjectCarsApp.css';
 import { Provider } from "react-redux";
 import ProjectCar from "./ProjectCar";
-import {store} from './store'
+import {store} from './store';
 
 function ProjectCarsApp() {
   return (
