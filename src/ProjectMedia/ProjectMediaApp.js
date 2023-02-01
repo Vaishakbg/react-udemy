099cc57907172dda08e0fcf85b2fd70a96e9fc9b
Vaishak <vaishakbg@gmail.com>
2023-02-01 05:34:35 +0000

    Store ans Slices

--- src/ProjectMedia/ProjectMediaApp.js
+++ src/ProjectMedia/ProjectMediaApp.js
@@ -1,9 +1,13 @@
 import './ProjectMediaApp.css';
 import ProjectMedia from "./ProjectMedia";
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function ProjectMediaApp() {
     return (
+        <Provider store={store}>
         <ProjectMedia />
+        </Provider>
     );
   }
   
