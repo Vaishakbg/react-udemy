722d8906e5f48258f4bd001f1178ff2c99bca66b
Vaishakbg <vaishakbg@gmail.com>
2023-02-19 14:04:00 +0530

    Creating Expandable Component

--- src/ProjectMedia/components/UserListItem.js
+++ src/ProjectMedia/components/UserListItem.js
@@ -1,28 +1,26 @@
 import { useThunk } from "../hooks/use-thunk";
 import { removeUser } from "../store";
-import { GoTrashcan } from 'react-icons/go';
+import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
 
 function UserListItem({ user }) {
   const [doremoveUser, isLoading, error] = useThunk(removeUser);
 
-  const handleClick = ()=> {
+  const handleClick = () => {
     doremoveUser(user);
-  }
-
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-        <Button loading={isLoading} onClick={handleClick} className="mr-3" danger>
-            <GoTrashcan />
-        </Button>
-        {error && <div>Error deleteing user</div>}
-        {user.name}
-        </div>
-      </div>
-    </div>
+  };
+  const header = (
+    <>
+      <Button loading={isLoading} onClick={handleClick} className="mr-3" danger>
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleteing user</div>}
+      {user.name}
+    </>
   );
+
+  return <ExpandablePanel header={header}>Content</ExpandablePanel>;
 }
 
 export default UserListItem;
