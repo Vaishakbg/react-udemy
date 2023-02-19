6e4713463cde5d725513a114ba761db60a963c0b
Vaishakbg <vaishakbg@gmail.com>
2023-02-19 13:50:56 +0530

    Fixing a delete Error

--- src/ProjectMedia/components/UserListItem.js
+++ src/ProjectMedia/components/UserListItem.js
@@ -13,11 +13,13 @@ function UserListItem({ user }) {
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
-        <Button loading={isLoading} onClick={handleClick}>
+        <div className="flex flex-row items-center justify-between">
+        <Button loading={isLoading} onClick={handleClick} className="mr-3" danger>
             <GoTrashcan />
         </Button>
         {error && <div>Error deleteing user</div>}
         {user.name}
+        </div>
       </div>
     </div>
   );
--- src/ProjectMedia/store/thunks/removeUser.js
+++ src/ProjectMedia/store/thunks/removeUser.js
@@ -2,10 +2,9 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const removeUser = createAsyncThunk('user/remove', async (user)=>{
-    const response = await axios.delete(`http://localhost:3001/users/${user.id}`);
+    await axios.delete(`http://localhost:3001/users/${user.id}`);
 
-    //FIX!!
-    return response.data;
+    return user;
 });
 
 export { removeUser };
